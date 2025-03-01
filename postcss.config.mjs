1cd50a37360f16f2af15ee74f52be5b335353b3a
Didier Nsengiyumva <nsedidier@gmail.com>
2025-03-02 01:23:20 +0200

    latets updates

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
-export default config;
+
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
+// export default config;
